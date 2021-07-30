668b5d7d1ea21b926a1a18791992dfee5bd0df7d
omar <nyirurugoomar@gmail.com>
2021-07-30 22:50:28 +0200

    js file added to file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-let
+let hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+let mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+let header =document.querySelector('.header.container');
+
+
+hamburger.addEventListener('click',() => {
+    hamburger.classList.toggle('active');
+});
